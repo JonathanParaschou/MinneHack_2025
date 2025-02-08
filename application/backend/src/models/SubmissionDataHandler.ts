f576ae301563bae92b5de840698686159fa07f03
Jonathan <paras044@umn.edu>
2025-02-08 17:11:30 -0600

    Submission CRUD

--- application/backend/src/models/SubmissionDataHandler.ts
+++ application/backend/src/models/SubmissionDataHandler.ts
@@ -1,6 +1,6 @@
 // Import necessary Firebase services
 import { FirebaseApp, initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs, addDoc, Firestore } from "firebase/firestore";
+import { getFirestore, collection, getDocs, addDoc, Firestore, deleteDoc, doc, updateDoc } from "firebase/firestore";
 import { firebaseConfig } from "../constants/firebaseConstants";
 import { SUBMISSION_COLLECTION } from "../constants/firebaseConstants";
 import { SubmissionInfo } from "interfaces/ISubmissionInfo";
@@ -17,6 +17,7 @@ export class SubmissionDataHandler {
         this.db = getFirestore(app);
     }
 
+    // READ
     async fetchData() {
         try {
             const querySnapshot = await getDocs(collection(this.db, SUBMISSION_COLLECTION));
@@ -28,11 +29,34 @@ export class SubmissionDataHandler {
         }
     }
 
+    // CREATE
     async addData(submissionData: SubmissionInfo) {
         try {
             await addDoc(collection(this.db, SUBMISSION_COLLECTION), submissionData);
         } catch (e) {
             throw new Error("Error adding document: " + e);
+        }
+    }
+
+    // DESTROY
+    async deleteData(id: string) {
+        try {
+            const docRef = doc(this.db, SUBMISSION_COLLECTION, id);
+            await deleteDoc(docRef);
+        } catch (e) {
+            throw new Error("Error deleting document: " + e);
+        }
+    }
+
+    // UPDATE
+    async updateData(id: string, updatedData: Partial<SubmissionInfo>) {
+        try {
+            const docRef = doc(this.db, SUBMISSION_COLLECTION, id);
+
+            await updateDoc(docRef, updatedData);
+            console.log(`Document with ID ${id} updated.`);
+        } catch (e) {
+            throw new Error("Error updating document: " + e);
         }
     }
 }
