d0786f7ac2c81f1d7365875694a20832879460f6
Peter Olsen <olse0321@umn.edu>
2025-02-08 18:01:38 -0600

    push to peter branch

--- application/frontend/app/(tabs)/index.tsx
+++ application/frontend/app/(tabs)/index.tsx
@@ -1,9 +1,54 @@
-import { Text, View } from "react-native";
+import { Text, View, Image, Dimensions } from "react-native";
+import { StyleSheet } from "react-native";
+
+
+const { width, height } = Dimensions.get('window');
 
 export default function Index() {
   return (
     <View>
-      <Text>INDEX PAGE INDEX PAGE</Text>
+      <View style={styles.header} >
+        <Image
+          source={require('../images/user-friends.svg')}
+          style={styles.friends}></Image>
+        <Text style={styles.title}>
+          DrawIt.
+        </Text>
+        <Image
+          source={require('../images/user.png')}
+          style={styles.user}></Image>
+      </View>
+
+
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  header: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    display: 'flex',
+    flexDirection: 'row',
+    borderBottomColor: '#000',
+    borderBottomWidth: 1,
+  },
+  friends: {
+    width: width * 0.08,
+    height: width * 0.08
+  },
+  user: {
+    height: width * 0.06,
+    width: width * 0.06,
+    borderRadius: 15,
+    filter: 'invert(1)'
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    flex: 1,
+    textAlign: 'center'
+  }
+});
