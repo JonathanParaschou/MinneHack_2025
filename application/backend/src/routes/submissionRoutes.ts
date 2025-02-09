a7cfec89f65a7833cc70065c533aea5d10613c30
Jonathan <paras044@umn.edu>
2025-02-08 18:01:17 -0600

    Submission API functions

--- application/backend/src/routes/submissionRoutes.ts
+++ application/backend/src/routes/submissionRoutes.ts
@@ -6,27 +6,27 @@ import { SubmissionDataHandler } from '../models/SubmissionDataHandler';
 const router = express.Router();
 const db = new SubmissionDataHandler();
 
-// CREATE
-router.post('/', async (req, res) => {
-    try {
-        await db.addData(req.body);
-        res.status(201).json();
-    } catch (err: any) {
-        res.status(500).json({ error: err.message });
-    }
-});
+// Fetching all submission data
+// router.post('/', async (req, res) => {
+//     try {
+//         await db.addData(req.body);
+//         res.status(201).json();
+//     } catch (err: any) {
+//         res.status(500).json({ error: err.message });
+//     }
+// });
 
 // READ
-router.get('/', async (req, res) => {
-    try {
-        await db.fetchData();
-        res.status(200).json(res);
-    } catch (err: any) {
-        res.status(500).json({ error: err.message });
-    }
-});
+// router.get('/', async (req, res) => {
+//     try {
+//         const submissions = await db.fetchData();
+//         res.status(200).json(JSON.stringify(submissions));
+//     } catch (err: any) {
+//         res.status(500).json({ error: err.message });
+//     }
+// });
 
-// UPDATE
+// Update data on a specific submission
 router.put('/:id', async (req, res) => {
     try {
         await db.updateData(req.params.id, req.body);
@@ -36,7 +36,7 @@ router.put('/:id', async (req, res) => {
     }
 });
 
-// DESTROY
+// Delete a submission based on id
 router.delete('/:id', async (req, res) => {
     try {
         await db.deleteData(req.params.id);
@@ -45,5 +45,26 @@ router.delete('/:id', async (req, res) => {
         res.status(500).json({ error: err.message });
     }
 });
+
+// Fetch all submissions based on creator ID
+router.get('/creator/:creatorIds', async (req, res) => {
+    try {
+        const submissions = await db.fetchSubmissionDataByCreatorIds([req.params.creatorIds]);
+        res.status(200).json(JSON.stringify(submissions));
+    } catch (err: any) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+// Fetch submission by ID
+router.get('/:id', async (req, res) => {
+    try {
+        const submission = await db.fetchSubmissionDataById(req.params.id);
+        res.status(200).json(JSON.stringify(submission));
+    } catch (err: any) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
 
 export default router;
